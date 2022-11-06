3bac08e7e07f1f6c1de1b7bdbc84fc2df5f8d3b1
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-11-06 14:22:13 -0500

    commit tree 3d60534575f5933f19b46cf53559f02892d7823f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  post "/signup", to: "users#create"
+  get "/me", to: "user#show"
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  see
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
